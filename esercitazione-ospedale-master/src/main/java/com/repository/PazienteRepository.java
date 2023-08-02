58b051959bfd916c785fe9dec2ec42b7bf1e934c
MicheleCe <michele.cerqua@hotmail.com>
2023-08-02 10:43:52 +0200

    no backEnd

--- esercitazione-ospedale-master/src/main/java/com/repository/PazienteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.entity.Paziente;
-
-public interface PazienteRepository extends JpaRepository<Paziente, Integer> {
-
-}
